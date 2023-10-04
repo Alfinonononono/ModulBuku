4722a82aa1dda4c9b62e9402e5d2c0c69d119f63
Alfino <alfinorianandha@gmail.com>
2023-10-04 09:31:20 +0700

    intial project

---
+++ src/main/java/org/example/main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.Buku;
+
+
+public class main
+{
+//    public static void main(String[] args)
+//    {
+//        Buku a = new Buku();
+//        a.setNilai("Jurassic Park", "Michael Crichton",21);
+//        a.cetakKeLayar();
+//    }
+
+    public static void main(String[] args)
+    {
+        Buku a,b = new Buku();
+        a = new Buku(" Siaga Merah "," Alistair Maclean ");
+        b = new Buku();
+        a.cetakKeLayar();
+        b.cetakKeLayar();
+    }
+}
